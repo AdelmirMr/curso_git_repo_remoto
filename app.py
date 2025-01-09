77427c953ae00808b4822ba28ea22534dba7c788
Adelmir Marinhoi <adelmirmarinho@gmail.com>
2025-01-08 22:50:13 -0300

    Arquivo app.py criado e inicializado

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from src.extraction import load_data
+
+st.set_page_config(layout = 'wide')
+def mais():
+    df_raw = load_data()
+    st.dataframe(df_raw)
+if __name__ == '__main__':
+    main()
